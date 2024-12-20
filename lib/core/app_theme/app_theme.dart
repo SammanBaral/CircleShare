3686265dc22cdd726de1cf04176a99cdd2906b92
Samman Baral <bulkedout.2@gmail.com>
2024-12-20 18:54:37 +0545

    final changes for sprint

--- lib/core/app_theme/app_theme.dart
+++ lib/core/app_theme/app_theme.dart
@@ -2,43 +2,34 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // Light theme primary color
     primaryColor: Colors.black,
-    scaffoldBackgroundColor: Colors.white, // Light background color
-
-    // Font family
+    scaffoldBackgroundColor: Colors.white,
     fontFamily: 'Montserrat Regular',
-
-    // AppBar theme
     appBarTheme: AppBarTheme(
-      backgroundColor: Colors.white, // Light AppBar background
+      backgroundColor: Colors.white,
       elevation: 1,
-      iconTheme: IconThemeData(color: Colors.black), // Icons in black
+      iconTheme: IconThemeData(color: Colors.black),
       titleTextStyle: TextStyle(
-        color: Colors.black, // Title text in black
+        color: Colors.black,
         fontSize: 20,
         fontWeight: FontWeight.bold,
         fontFamily: 'Montserrat Regular',
       ),
     ),
-
-    // ElevatedButton theme
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         textStyle: const TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w500,
         ),
-        foregroundColor: Colors.white, // Text in white
-        backgroundColor: Colors.black, // Black button background
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     ),
-
-    // Input decoration theme
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
-      fillColor: Colors.grey[100], // Light background for input fields
+      fillColor: Colors.grey[100],
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
         borderSide: BorderSide.none,
@@ -47,14 +38,12 @@ ThemeData getApplicationTheme() {
         borderRadius: BorderRadius.circular(8),
         borderSide: BorderSide(color: Colors.black, width: 1.5),
       ),
-      hintStyle: TextStyle(color: Colors.grey[600]), // Hint text color
+      hintStyle: TextStyle(color: Colors.grey[600]),
       contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 16),
     ),
-
-    // TextButton theme
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
-        foregroundColor: Colors.black, // Text in black
+        foregroundColor: Colors.black,
         textStyle: TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w500,
@@ -62,44 +51,36 @@ ThemeData getApplicationTheme() {
         ),
       ),
     ),
-
-    // Floating Action Button theme
     floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: Colors.black, // Black FAB background
-      foregroundColor: Colors.white, // White FAB icon
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
     ),
-
-    // Card theme
     cardTheme: CardTheme(
       margin: EdgeInsets.all(8),
       elevation: 2,
-      color: Colors.white, // Light card background
+      color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
       ),
     ),
-
-    // Icon theme
     iconTheme: IconThemeData(
-      color: Colors.black, // Black icons
+      color: Colors.black,
     ),
-
-    // Text theme
     textTheme: TextTheme(
       displayLarge: TextStyle(
         fontSize: 24,
         fontWeight: FontWeight.bold,
-        color: Colors.black, // Black text
+        color: Colors.black,
         fontFamily: 'Montserrat Regular',
       ),
       headlineMedium: TextStyle(
         fontSize: 20,
         fontWeight: FontWeight.w600,
-        color: Colors.black, // Headline text in black
+        color: Colors.black,
       ),
       bodyLarge: TextStyle(
         fontSize: 16,
-        color: Colors.grey[800], // Body text in dark grey
+        color: Colors.grey[800],
       ),
     ),
   );
